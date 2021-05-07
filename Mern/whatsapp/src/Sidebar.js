69095e5030246e7569b1161192e594a688a52a89
akashsmu <smu.akash9@gmail.com>
2021-05-07 08:52:03 +0530

    Frontend functionality added

---
+++ Mern/whatsapp/src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar_header">
+        <div className="sidebar_headerRight"></div>
+        <div className="sidebar_headerLeft"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
