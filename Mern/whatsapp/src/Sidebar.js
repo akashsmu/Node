ef1a3a88762a24ec936afc04260e0aeffcef1540
akashsmu <smu.akash9@gmail.com>
2021-05-10 08:05:59 +0530

    Added Sidebar

--- Mern/whatsapp/src/Sidebar.js
+++ Mern/whatsapp/src/Sidebar.js
@@ -1,12 +1,41 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  ChatOutlined,
+  DonutLargeOutlined,
+  MoreVert,
+  SearchOutlined,
+} from "@material-ui/icons";
 import React from "react";
 import "./Sidebar.css";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar_header">
-        <div className="sidebar_headerRight"></div>
-        <div className="sidebar_headerLeft"></div>
+        <Avatar />
+        <div className="sidebar_headerRight">
+          <IconButton>
+            <DonutLargeOutlined />
+          </IconButton>
+          <IconButton>
+            <ChatOutlined />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar_search">
+        <div className="sidebar_searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+
+      <div className="sidebar_chat">
+        <SidebarChat />
       </div>
     </div>
   );
