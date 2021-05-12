4e1f3090ae72407cbd04867f5490716b5fbfca23
akashsmu <smu.akash9@gmail.com>
2021-05-12 08:32:46 +0530

    chat body added

--- Mern/whatsapp/src/Chat.js
+++ Mern/whatsapp/src/Chat.js
@@ -1,8 +1,39 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import React from "react";
 import "./Chat.css";
 
 function Chat() {
-  return <div>sfbbsdddsifbsdjncknn</div>;
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar />
+        <div className="chat_headerInfo">
+          <h3> Room Name</h3>
+          <p> Last Seen At</p>
+        </div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat_body">
+        <p>
+          <span className="chat_name">Akash</span>
+          This is the message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
