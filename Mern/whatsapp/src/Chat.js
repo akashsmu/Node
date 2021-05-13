b1a1f96869d1d49b49fdd73e7003d09ce39dc35e
akashsmu <smu.akash9@gmail.com>
2021-05-13 08:25:07 +0530

    Added chat body

--- Mern/whatsapp/src/Chat.js
+++ Mern/whatsapp/src/Chat.js
@@ -1,9 +1,18 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
-import React from "react";
+import {
+  AttachFile,
+  InsertEmoticon,
+  MicOutlined,
+  MoreVert,
+  SearchOutlined,
+} from "@material-ui/icons";
+import React, { useState } from "react";
 import "./Chat.css";
 
 function Chat() {
+  const [input, setInput] = useState();
+
+  const sendMessage = () => {};
   return (
     <div className="chat">
       <div className="chat_header">
@@ -26,11 +35,27 @@ function Chat() {
       </div>
 
       <div className="chat_body">
-        <p>
+        <p className="chat_message chat_reciever">
           <span className="chat_name">Akash</span>
           This is the message
           <span className="chat_timestamp">{new Date().toUTCString()}</span>
         </p>
+      </div>
+
+      <div className="chat_footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            value={input}
+            onchange={(e) => setInput(e.target.value)}
+            placeholder="Type a message"
+            type="text"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a Message
+          </button>
+        </form>
+        <MicOutlined />
       </div>
     </div>
   );
