8a238154376746899412b2edff766026e392dc33
akashsmu <smu.akash9@gmail.com>
2021-05-25 08:13:36 +0530

    Added Pusher

--- Mern/whatsapp-backend/server.js
+++ Mern/whatsapp-backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
+import Pusher from "pusher";
 
 const app = express();
 const port = process.env.PORT || 9000;
@@ -16,6 +17,21 @@ mongoose.connect(url, {
 app.use(express.json());
 
 app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+const pusher = new Pusher({
+  appId: "1209087",
+  key: "39e0484560960626b5c3",
+  secret: "8d8963cd6e8da4535226",
+  cluster: "ap2",
+  useTLS: true,
+});
+
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("DB connected");
+  const msgCollection = db.collection("messagecontent");
+  const changeStream = msgCollection.watch();
+});
 
 app.get("/messages/sync", (req, res) => {
   Messages.find((err, data) => {
